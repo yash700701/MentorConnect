29b189a387f463860378800086a3a08d7fb6617e
agrim08 <agrimgupta0805@gmail.com>
2024-08-26 15:25:26 +0530

    sign up error solved

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA_lb33IdxiF8bodY5oP-Cofg4_oCDpya0",
-    authDomain: "mentorconnect-2ca0e.firebaseapp.com",
-    databaseURL: "https://mentorconnect-2ca0e-default-rtdb.firebaseio.com",
-    projectId: "mentorconnect-2ca0e",
-    storageBucket: "mentorconnect-2ca0e.appspot.com",
-    messagingSenderId: "819171036234",
-    appId: "1:819171036234:web:67d37ba85543b03402eab3",
-    measurementId: "G-VHB1W6YY2Y"
-  };
+  apiKey: "AIzaSyA_lb33IdxiF8bodY5oP-Cofg4_oCDpya0",
+  authDomain: "mentorconnect-2ca0e.firebaseapp.com",
+  databaseURL: "https://mentorconnect-2ca0e-default-rtdb.firebaseio.com",
+  projectId: "mentorconnect-2ca0e",
+  storageBucket: "mentorconnect-2ca0e.appspot.com",
+  messagingSenderId: "819171036234",
+  appId: "1:819171036234:web:67d37ba85543b03402eab3",
+  measurementId: "G-VHB1W6YY2Y",
+};
 
-  // initialize firebase
-  firebase.initializeApp(firebaseConfig);
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
 
-  //reference your database
-  var signupFormDB = firebase.database().ref('signupForm')
-  document.getElementById('signupForm').addEventListener("submit" , submitForm)
+//reference your database
+let signupFormDB = firebase.database().ref("signupForm");
+document.getElementById("signupForm").addEventListener("submit", submitForm);
 
-  function submitForm(e){
-    e.preventDefault();
-    var Username = getElementVal('Username');
-    var Email = getElementVal('Email');
-    var Password = getElementVal('Password');
+function submitForm(e) {
+  e.preventDefault();
+  let Username = getElementVal("username");
+  let Email = getElementVal("email");
+  let Password = getElementVal("password");
 
-    console.log(Username,Email,Password);
-  }
+  console.log(Username, Email, Password);
+}
 
-  const getElementVal = (id) => {
-      return document.getElementById(id).value;
-  };
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
